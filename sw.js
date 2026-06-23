1d6a25a24a325e344b09fbf7c6b81194b32d2193
baltop111 <baltop111@gmail.com>
2026-06-23 13:18:37 +0900

    서비스워커 완전제거 + 캐시 무효화 - 항상 최신버전 강제로드

--- sw.js
+++ sw.js
@@ -1,30 +1,9 @@
-const CACHE = 'flirt-coach-v5';
-const ASSETS = ['./', './index.html', './manifest.json'];
-
-// 설치: 캐시 저장
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-// 활성화: 이전 버전 캐시 전부 삭제
+// 모든 이전 캐시 삭제 및 서비스워커 비활성화
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
   );
 });
-
-// 네트워크 우선 전략: 인터넷 되면 항상 최신 파일, 오프라인이면 캐시
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        var clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-        return res;
-      })
-      .catch(() => caches.match(e.request))
-  );
-});
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
