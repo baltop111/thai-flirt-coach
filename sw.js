28bee9f13b5b78c8a462ef2812c9d5d5036592fd
baltop111 <baltop111@gmail.com>
2026-06-23 13:14:56 +0900

    서비스워커 네트워크우선 전략으로 변경 - 항상 최신버전 보장

--- sw.js
+++ sw.js
@@ -1,20 +1,30 @@
-const CACHE = 'flirt-coach-v3';
+const CACHE = 'flirt-coach-v5';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
+// 설치: 캐시 저장
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
 
+// 활성화: 이전 버전 캐시 전부 삭제
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
 });
 
+// 네트워크 우선 전략: 인터넷 되면 항상 최신 파일, 오프라인이면 캐시
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./index.html')))
+    fetch(e.request)
+      .then(res => {
+        var clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
   );
 });
